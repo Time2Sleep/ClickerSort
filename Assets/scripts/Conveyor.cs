688c522a8494d5054a20209c465a9203c4b3bcea
Airat Zaripov <omg.airat@hotmail.com>
2019-10-06 20:25:48 +0300

    Points and Trucks

---
+++ Assets/scripts/Conveyor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    public float speed = 3f;
+
+    private void OnCollisionStay(Collision other)
+    {
+        Debug.Log("Collision stay" + other.gameObject.name);
+        other.transform.Translate(transform.right*speed*Time.deltaTime, Space.World);
+    }
+}
