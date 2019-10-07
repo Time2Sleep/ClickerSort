1e9e2e412d99dc956b62cd84ff8a1d478c3e5dfa
Airat Zaripov <omg.airat@hotmail.com>
2019-10-07 22:01:05 +0300

    Conveyor Belt animation

--- Assets/scripts/Conveyor.cs
+++ Assets/scripts/Conveyor.cs
@@ -6,9 +6,16 @@ public class Conveyor : MonoBehaviour
 {
     public float speed = 3f;
     public ItemType expectedType;
-    
+    [SerializeField]private Renderer conveyorBelt;
+    float conveyorOffset = 0f;
     private void OnCollisionStay(Collision other)
     {
         other.transform.Translate(Time.deltaTime * speed * transform.right, Space.World);
+    }
+
+    void FixedUpdate()
+    {
+        conveyorOffset -= speed / 2f;
+        conveyorBelt.material.SetTextureOffset("_MainTex", new Vector2(0, conveyorOffset * Time.deltaTime));
     }
 }
